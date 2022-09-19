547e44d0579f1a91746843c999abc5245a651eff
GreXLin85 <grexlin85@gmail.com>
2022-09-20 01:34:20 +0300

    Removal of logs in cache

--- src/helpers/Middleware/PrismaCache.ts
+++ src/helpers/Middleware/PrismaCache.ts
@@ -2,14 +2,8 @@ import { createPrismaRedisCache } from "prisma-redis-middleware";
 import redis from "../Connector/Redis";
 
 const cacheMiddleware = createPrismaRedisCache({
-  storage: { type: "redis", options: { client: redis, invalidation: { referencesTTL: 300 }, log: console } },
+  storage: { type: "redis", options: { client: redis, invalidation: { referencesTTL: 300 } } },
   cacheTime: 300,
-  onHit: (key) => {
-    console.log("hit", key);
-  },
-  onMiss: (key) => {
-    console.log("miss", key);
-  },
   onError: (key) => {
     console.log("error", key);
   },
