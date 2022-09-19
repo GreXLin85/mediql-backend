7673a6e36067eab3228e2c681f13d356309805d0
GreXLin85 <grexlin85@gmail.com>
2022-09-20 01:14:41 +0300

    Post resolver is ready

--- src/modules/Post/resolver.ts
+++ src/modules/Post/resolver.ts
@@ -1,10 +1,87 @@
 import { ApolloError, AuthenticationError, ForbiddenError } from 'apollo-server-express';
+import { Context } from '../../helpers/ContextBuilder';
+
+interface PostCreateInput {
+    title: string;
+    content: string;
+    authorId: number;
+    published: boolean;
+}
+interface PostUpdateInput extends PostCreateInput {
+    viewCount: number;
+}
 
 export default {
     Mutation: {
+        createPost: async (_: any, args: { post: PostCreateInput }, context: Context) => {
+            const { post } = args;
+            const newPost = await context.prisma.post.create({
+                data: {
+                    title: post.title,
+                    content: post.content,
+                    published: post.published,
+                    authorId: post.authorId,
+                },
+            });
+            return newPost;
+        },
+        updatePost: async (_: any, args: { id: number; post: Partial<PostUpdateInput> }, context: Context) => {
+            const { id, post } = args;
+            const updatedPost = await context.prisma.post.update({
+                where: { id },
+                data: post,
+            });
+            return updatedPost;
+        },
+        deletePost: async (_: any, args: { id: number }, context: Context) => {
+            const { id } = args;
+            const deletedPost = await context.prisma.post.delete({
+                where: { id },
+            });
+            return deletedPost;
+        },
     },
     Query: {
+        posts: async (_: any, args: { skip: number; take: number }, context: Context) => {
+            const { skip, take } = args;
+            const posts = await context.prisma.post.findMany({
+                skip,
+                take,
+            });
+            return posts;
+        },
+        post: async (_: any, args: { id: number }, context: Context) => {
+            const { id } = args;
+            const post = await context.prisma.post.findUnique({
+                where: { id },
+            });
+            return post;
+        }
     },
     Post: {
+        likeCount: async (parent: any, args: any, context: Context) => {
+            // Count of posts likes
+            const likes = await context.prisma.post.findUnique({
+                select: {
+                    _count: {
+                        select: { likes: true },
+                    },
+                },
+                where: { id: parent.id },
+            })
+            
+            return likes?._count.likes;
+        },
+        likedBy: async (parent: any, args: any, context: Context) => {
+            // Users who liked the post
+            const likedBy = await context.prisma.post.findUnique({
+                where: {
+                    id: parent.id
+                }
+            }).likes();
+
+            return likedBy;
+        },
+
     },
 };
