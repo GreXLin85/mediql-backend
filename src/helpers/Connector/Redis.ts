edbd4d31a45fe6b900390014806c6893843af298
GreXLin85 <grexlin85@gmail.com>
2022-09-29 12:12:43 +0300

    Temp fix for connection issue dotenv was not loading env's to Redis.ts file from main server.ts file

--- src/helpers/Connector/Redis.ts
+++ src/helpers/Connector/Redis.ts
@@ -1,4 +1,8 @@
+import * as dotenv from 'dotenv'
+import { join } from "path";
 import Redis from "ioredis";
+// temp fix for connection issues
+dotenv.config({ path: join(__dirname, "/../../.env") });
 
 const redis = new Redis({
     host: process.env.REDIS_HOST,
