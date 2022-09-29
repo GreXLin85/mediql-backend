d14dd8429789928a503043a5cf2c8b7183540808
GreXLin85 <grexlin85@gmail.com>
2022-09-29 12:09:59 +0300

    Redis based session auth

--- src/helpers/ContextBuilder.ts
+++ src/helpers/ContextBuilder.ts
@@ -1,15 +1,38 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient, User } from "@prisma/client"
 import JWTHelper from "./JWTHelper"
+import SessionHelper from "./SessionHelper"
 import prisma from "./Connector/Database"
 import redis from "./Connector/Redis"
 export interface Context {
     prisma: PrismaClient
     redis: typeof redis
+    session: typeof SessionHelper
     jwt: typeof JWTHelper
+    user: User | null
 }
 
-export interface ContextFunc { (): Context }
+export interface ContextFunc { ({ req }: { req: any }): Promise<Context> }
 
-export const context = (): Context => {
-    return { prisma: prisma, redis: redis, jwt: JWTHelper }
+export const context = async ({ req }: { req: any }): Promise<Context> => {
+    // @ts-ignore
+    let user = null
+    if (req.body.operationName == 'IntrospectionQuery') {
+        // @ts-ignore
+        return {}
+    }
+    if (req.headers.authorization) {
+        const authKey = req.headers.authorization.split(" ")[1]
+
+        const userId = (await SessionHelper.verify(authKey))?.id
+
+        if (userId) {
+            user = await prisma.user.findUnique({
+                where: {
+                    id: userId
+                }
+            })
+        }
+    }
+
+    return { prisma, redis, session: SessionHelper, jwt: JWTHelper, user }
 }
