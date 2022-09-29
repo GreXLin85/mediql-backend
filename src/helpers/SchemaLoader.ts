2fe1a873dedc2fc41ddf76c918bcf7a95ab62f21
GreXLin85 <grexlin85@gmail.com>
2022-09-29 12:10:24 +0300

    New directives added to loader file #2 #3

--- src/helpers/SchemaLoader.ts
+++ src/helpers/SchemaLoader.ts
@@ -4,6 +4,10 @@ import { join } from 'path'
 import { loadFilesSync } from '@graphql-tools/load-files'
 import { loadSchemaSync } from '@graphql-tools/load'
 import { mergeResolvers } from '@graphql-tools/merge'
+import AuthenticationDirective from './Directives/Authentication'
+import DirectiveBuilder from './DirectiveBuilder'
+import IsLoggedInDirective from './Directives/IsLoggedIn'
+import IsOnlyCanSeeSelfDirective from './Directives/IsOnlyCanSeeSelf'
 
 const schema = loadSchemaSync(join(__dirname, '/../modules/**/schema.graphql'), { loaders: [new GraphQLFileLoader()] })
 const resolverFiles = loadFilesSync(join(__dirname, '/../modules/**/resolver.ts'))
@@ -11,4 +15,10 @@ const resolverFiles = loadFilesSync(join(__dirname, '/../modules/**/resolver.ts'
 const resolvers = mergeResolvers(resolverFiles);
 const schemaWithResolvers = addResolversToSchema({ schema, resolvers })
 
-export default schemaWithResolvers
+// Add directives to schema
+// TODO - Make a loader for directives
+const schemaWithAuthDirective = DirectiveBuilder(schemaWithResolvers, 'auth', AuthenticationDirective);
+const schemaWithIsLoggedInDirective = DirectiveBuilder(schemaWithAuthDirective, 'isLoggedIn', IsLoggedInDirective);
+const schemaWithIsOnlyCanSeeSelfDirective = DirectiveBuilder(schemaWithIsLoggedInDirective, 'isOnlyCanSeeSelf', IsOnlyCanSeeSelfDirective);
+
+export default schemaWithIsOnlyCanSeeSelfDirective
