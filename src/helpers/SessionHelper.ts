87b0c2c587832daba2d4aff6099bf87c4bbb8b73
GreXLin85 <grexlin85@gmail.com>
2022-09-29 12:25:32 +0300

    Performance optimization

--- src/helpers/SessionHelper.ts
+++ src/helpers/SessionHelper.ts
@@ -6,16 +6,20 @@ const EXPIRE_TIME = 60 * 60 * 24 * 7; // 7 days
 export default {
     sign: async (id: number) => {
         const isSigned = await redis.get(`user:${id}:authtoken`);
-        if (isSigned !== null){
+        if (isSigned !== null) {
             return isSigned;
         }
 
         const token = nanoid.nanoid(32);
-        const setToken = await redis.set(`authtoken:${token}`, id, 'EX', EXPIRE_TIME);
-        const addToken = await redis.set(`user:${id}:authtoken`, token, 'EX', EXPIRE_TIME);
-        if (setToken === 'OK' && addToken === "OK") {
+        const setToken = await redis.pipeline()
+            .set(`authtoken:${token}`, id, 'EX', EXPIRE_TIME)
+            .set(`user:${id}:authtoken`, token, 'EX', EXPIRE_TIME)
+            .exec() as Array<[Error, 'OK']>;
+
+        if (setToken[0][1] === "OK" || setToken[1][1] === "OK") {
             return token;
         }
+        throw setToken;
     },
     verify: async (token: string): Promise<{ id: number; } | null> => {
         const id = Number(await redis.get(`authtoken:${token}`));
