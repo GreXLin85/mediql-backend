a49970274c3e2960eced456a06b1052fc7af5bba
GreXLin85 <grexlin85@gmail.com>
2022-10-18 10:50:07 +0300

    Added automatic resolving for login and signup operations

--- src/helpers/Directives/IsLoggedIn.ts
+++ src/helpers/Directives/IsLoggedIn.ts
@@ -1,18 +1,20 @@
 import { AuthenticationError } from 'apollo-server';
 import { Context } from '../ContextBuilder';
 import { GraphQLFieldResolver, GraphQLResolveInfo } from 'graphql';
+// NOTE -
+// Write a helper for admin authentication that can be used in the same way as the Authentication directive
 
-export default (params: Array<Record<string, any>> | undefined, resolve: GraphQLFieldResolver<any, any, any, unknown>) => {
+export default (params: Array<Record<string, any>> | undefined, resolve: GraphQLFieldResolver<any, any, Context, GraphQLResolveInfo>) => {
   return async function (source: any, args: any, context: Context, info: GraphQLResolveInfo) {
     const operationName = info.operation.name?.value.toLowerCase();
     if (operationName === 'login' || operationName === 'signup') {
-      return await resolve(source, args, context, info);
+      return resolve(source, args, context, info);
     }
 
     if (!context.user) {
       throw new AuthenticationError('You must be logged in to access this resource');
     }
 
-    return await resolve(source, args, context, info);
+    return resolve(source, args, context, info);
   }
 }
